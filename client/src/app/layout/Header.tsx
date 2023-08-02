3c7329218bddb0792094b92cbd49b5eef96c5640
3b3n3z3r666 <nickfraser40@gmail.com>
2023-08-02 18:56:29 -0400

    End of section 4

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -1,22 +1,90 @@
-import { AppBar, Switch, Toolbar, Typography } from "@mui/material";
+import { ShoppingCart } from "@mui/icons-material";
+import { AppBar, Badge, Box, IconButton, List, ListItem, Switch, Toolbar, Typography } from "@mui/material";
+import { NavLink } from "react-router-dom";
+
+
+const midLinks = [
+    {title: 'catalog', path : '/catalog'},
+    {title: 'about', path : '/about'},
+    {title: 'contact', path : '/contact'},
+]
+
+const rightLinks = [
+    {title: 'login', path : '/login'},
+    {title: 'register', path : '/register'},
+]
 
 interface Props {
     darkMode: boolean;
     handleThemeChange: () => void;
 }
 
+const navStyles = {
+    color:'inherit',
+    textDecoration:'none', 
+    typography:'h6', 
+    '&:hover':{color:'grey.500'},
+    '&.active': {color: 'text.secondary'}
+}
+
 export default function Header({darkMode, handleThemeChange} : Props){
-    var darkModeText = darkMode ? "dark mode": 'light mode'
+    var darkModeText = darkMode ? "DARK MODE": "LIGHT MODE"
     return(
         <AppBar position="static" sx={{mb: 4}}>
-            <Toolbar>
-                <Typography variant="h6">
-                    Web Store
-                </Typography>
-                <Switch checked={darkMode} onChange={handleThemeChange} />
-                <Typography variant="h6" sx={{color: darkMode ? 'secondary.light' : 'lightgrey' }}>
-                {darkModeText}
-                </Typography>
+            <Toolbar sx={{display:'flex', justifyContent:'space-between', alignItems:'center'}}>
+
+                <Box display='flex' alignItems='center'> 
+                    <Typography     
+                        variant="h6" 
+                        component={NavLink} to='/' 
+                        sx={navStyles}
+                    >
+                        WEB STORE
+                    </Typography>
+                        
+                        <Switch checked={darkMode} onChange={handleThemeChange} />
+                        <Typography 
+                            variant="h6" 
+                            sx={{color: darkMode ? 'secondary.light' : 'eaeaea', 
+                            fontSize: 8 ,
+                            textAlign:'center',
+                            padding:1
+                            }}
+                            >
+                            {darkModeText}
+                        </Typography>
+                </Box>
+                
+                
+                <List sx={{display: 'flex'}}>
+                    {midLinks.map(({title, path}) => (
+                        <ListItem 
+                            component={NavLink} to={path} key={path} 
+                            sx={navStyles}
+                            >
+                            {title.toUpperCase()}
+                        </ListItem>
+                    ))}
+                </List>
+                
+                <Box display="flex" alignItems="center">
+                    <IconButton size='large' edge='start' color='inherit' sx={navStyles}>
+                        <Badge badgeContent='4' color='secondary'>
+                            <ShoppingCart sx={{}}/>
+                        </Badge>
+                    </IconButton>
+                    <List sx={{display: 'flex'}}>
+                        {rightLinks.map(({title, path}) => (
+                            <ListItem 
+                                component={NavLink} to={path} key={path} 
+                                sx={navStyles}>
+                                {title.toUpperCase()}
+                            </ListItem>
+                        ))}
+                    </List>
+                </Box>
+                
+                
             </Toolbar>
             
         </AppBar>
