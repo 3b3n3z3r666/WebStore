3ed5e2fc90fa255169f878684f67e008cd0b49ea
3b3n3z3r666 <nickfraser40@gmail.com>
2023-08-21 14:29:27 -0400

    End of Section 8. Filtering, sorting, pagination, skeleton loaders

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,9 +1,13 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
+using API.Extensions;
+using API.RequestHelpers;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -19,9 +23,21 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<List<Product>>> GetProducts()
+        public async Task<ActionResult<PagedList<Product>>> GetProducts(
+            [FromQuery] ProductParams productParams
+        )
         {
-            return await _context.Products.ToListAsync();
+            var query = _context.Products
+            .Sort(productParams.OrderBy)
+            .Search(productParams.SearchTerm)
+            .Filter(productParams.Brands, productParams.Types)
+            .AsQueryable();
+            var products = await PagedList<Product>.ToPagedList(
+                query, productParams.PageNumber, productParams.PageSize);
+
+
+            Response.AddPaginationHeader(products.MetaData);
+            return products;
         }
 
         [HttpGet]
@@ -36,5 +52,15 @@ namespace API.Controllers
             }
             return product;
         }
+
+        [HttpGet("filters")]
+        public async Task<IActionResult> GetFilters()
+        {
+            var brands = await _context.Products.Select(p => p.Brand).Distinct().ToListAsync();
+            var types = await _context.Products.Select(p => p.Type).Distinct().ToListAsync();
+
+            return Ok(new { brands, types });
+        }
+
     }
 }
