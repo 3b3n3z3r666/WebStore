0157d81e904ccbe04daad8f0eebbdddf990e464d
3b3n3z3r666 <nickfraser40@gmail.com>
2023-12-06 13:47:46 -0500

    Get API to run client build successfully

--- API/Program.cs
+++ API/Program.cs
@@ -87,6 +87,10 @@ if (app.Environment.IsDevelopment())
     });
 }
 
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
 app.UseCors(opt =>
 {
     opt.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins("http://localhost:3000");
@@ -96,6 +100,7 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+app.MapFallbackToController("Index","Fallback");
 
 var scope = app.Services.CreateScope();
 var context = scope.ServiceProvider.GetRequiredService<StoreContext>();
